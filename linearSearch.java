616b10023d203df67a4f8588dabaefe71bf9685e
vikramlance <vikram.lance@gmail.com>
2016-03-22 23:21:59 -0500

    new program

---
+++ linearSearch.java
@@ -0,0 +1,21 @@
+int[] A = {2,3,412,12,23,5,63,124,54,12,43,541,1,34} ;
+
+	
+	
+	function findIndex(values, target)
+ {
+   for(var i = 0; i < values.length; ++i)
+     {
+       if (values[i] == target) 
+         {       
+           return i; 
+         }
+     }
+   return -1;
+ }
+	
+	
+ 
+  System.out.println(i); 
+ 
+ 
