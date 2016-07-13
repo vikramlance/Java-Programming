30f3845e2b7110b936a617a194d6f58bab55b807
vikramlance <vikram.lance@gmail.com>
2016-07-12 23:41:49 -0500

    Create java-stdin-and-stdout-1.java

---
+++ HackerRank-Domain/Introduction/java-stdin-and-stdout-1.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class shout {
+
+    public static void main(String[] args) {
+
+        Scanner my_scan = new Scanner(System.in);
+
+        int a = my_scan.nextInt();
+        int b = my_scan.nextInt();
+        int c = my_scan.nextInt();
+
+
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+
+
+    }
+
+
+
+
+}
