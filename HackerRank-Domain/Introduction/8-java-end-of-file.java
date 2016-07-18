2a2bb7f72e7dc09af75c196571e595563d285d54
vikramlance <vikram.lance@gmail.com>
2016-07-17 20:14:34 -0500

    Create 8-java-end-of-file.java

---
+++ HackerRank-Domain/Introduction/8-java-end-of-file.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        
+        Scanner sc = new Scanner(System.in);
+        int x=1;
+        while ( sc.hasNext() ) {
+         String s= sc.nextLine();  
+         System.out.printf("%d %s\n", x,s);
+         x++;         
+    
+            
+            
+        }
+        
+        
+        
+        }
+    }
