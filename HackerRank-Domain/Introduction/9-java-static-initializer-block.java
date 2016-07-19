d4ead1da32e01de021a1436acc33ba934e0409f7
vikramlance <vikram.lance@gmail.com>
2016-07-18 20:51:19 -0500

    Create 9-java-static-initializer-block.java

---
+++ HackerRank-Domain/Introduction/9-java-static-initializer-block.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+  
+ static boolean flag;        
+ static int B;
+ static int H;
+static {Scanner sc = new Scanner(System.in);  
+    flag = false; 
+B = sc.nextInt();
+    H = sc.nextInt();
+if (B>0 && H>0) {
+    
+     flag=true;
+       
+}
+else 
+    {System.out.println( "java.lang.Exception: Breadth and height must be positive");}
+       }
+       
+       
+      public static void main(String[] args){
+		if(flag){
+			int area=B*H;
+			System.out.print(area);
+		}
+		
+	}//end of main
+
+}//end of class
