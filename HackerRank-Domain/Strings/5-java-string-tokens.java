e5ea05f484bea61c5f686d11f9431222d5eb0dea
vikramlance <vikram.lance@gmail.com>
2016-07-27 18:29:16 -0500

    Create 5-java-string-tokens.java

---
+++ HackerRank-Domain/Strings/5-java-string-tokens.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String s = scan.nextLine();
+        scan.close();
+        StringTokenizer st = new StringTokenizer(s , " !,?._'@");
+        
+        System.out.println(st.countTokens() );
+        while(st.hasMoreTokens())
+{
+System.out.println(st.nextToken());
+}
+    }
+}
