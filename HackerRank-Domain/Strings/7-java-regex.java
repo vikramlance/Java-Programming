0d9e62d9edc8da0cd6ce73e16a4ea3af047fa8cd
vikramlance <vikram.lance@gmail.com>
2016-07-29 21:22:46 -0500

    Update 7-java-regex.java

--- HackerRank-Domain/Strings/7-java-regex.java
+++ HackerRank-Domain/Strings/7-java-regex.java
@@ -16,50 +16,7 @@ class Solution{
     }
 }
 
-
-public static void regexChecker(String theRegex, String str2Check){
-
-// You define your regular expression (REGEX) using Pattern
-
-Pattern checkRegex = Pattern.compile(theRegex);
-
-// Creates a Matcher object that searches the String for
-
-// anything that matches the REGEX
-
-
-
-Matcher regexMatcher = checkRegex.matcher( str2Check );
-
-
-
-// Cycle through the positive matches and print them to screen
-
-// Make sure string isn't empty and trim off any whitespace
-
-
-
-while ( regexMatcher.find() ){
-
-if (regexMatcher.group().length() != 0){
-
-System.out.println( regexMatcher.group().trim() );
-
-
-
-// You can get the starting and ending indexs
-
-
-
-//System.out.println( "Start Index: " + regexMatcher.start());
-//System.out.println( "Start Index: " + regexMatcher.end());
-
-}
-
-}
-
-
-
-System.out.println();
-
-}
+class myRegex{
+    String pat = "(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)";
+       public String pattern = ("^(?:"+ pat +"\\.){3}" + pat + "$");
+    }
